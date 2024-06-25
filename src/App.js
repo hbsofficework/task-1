bfb7b5f115d444ad31879bce8309b468fc4cd514
Hasan Sohail <hbs.officework@gmail.com>
2024-06-25 15:46:33 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -7,23 +7,24 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TextConverter from './components/TextConverter';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 // import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 
 function App() {
-  
+
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert =(message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     })
     setTimeout(() => {
       setAlert(null);
-        }, 2000);
+    }, 2000);
   }
 
   // const removeBodyClasses = ()=>{
@@ -34,54 +35,54 @@ function App() {
   //   document.body.classList.remove('bg-primary')
   // }
 
-                      //clss
-    const toggleMode= ()=>{
-      // removeBodyClasses();
-      // console.log(cls);
-      // document.body.classList.add('bg-'+ cls) 
-      if (mode === 'light') {
-        setMode('dark');
-        document.body.style.backgroundColor = '#160040';
-        showAlert("Dark mode has been enabled", "success");
-      }
-      else{
-        setMode('light');
-        document.body.style.backgroundColor = 'White';
-        showAlert("Light mode has been enabled", "success");
-      } 
+  //clss
+  const toggleMode = () => {
+    // removeBodyClasses();
+    // console.log(cls);
+    // document.body.classList.add('bg-'+ cls) 
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#160040';
+      showAlert("Dark mode has been enabled", "success");
     }
-  
-  return ( 
-  <>
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'White';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
 
-<Navbar title="MyApp" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+  return (
+    <>
+      <TextConverter />
+      <Navbar title="MyApp" aboutText="About" mode={mode} toggleMode={toggleMode} />
 
-<Alert alert={alert}/>
-    <div className="container my-3">
+      <Alert alert={alert} />
+      <div className="container my-3">
 
-{/* users-->- Componet 1 
---.users/home--> componet 2
+        {/* users-->- Component 1 
+--.users/home--> component 2
 */}
-      {/* <BrowserRouter>
+        {/* <BrowserRouter>
         <Routes> */}
-               {/* <Route path='/about' element={<About/>}/> */}
-          {/* <Route path='/' element={ */}
-          <Textform showAlert={showAlert} heading="Enter the text for UPPER CASE" mode={mode}/>
-          {/* }/> */}
-     
-          
+        {/* <Route path='/about' element={<About/>}/> */}
+        {/* <Route path='/' element={ */}
+        <Textform showAlert={showAlert} heading="Enter the text for UPPER CASE" mode={mode} />
+        {/* }/> */}
 
-        
+
+
+
         {/* </Routes>
 
       </BrowserRouter> */}
-       
-      
 
-    </div>
-  </>
 
-    );
-  }
+
+      </div>
+    </>
+
+  );
+}
 
 export default App;
---
+++ src/components/TextConverter.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+function TextConverter() {
+    const [inputText, setInputText] = useState('');
+    const [convertedText, setConvertedText] = useState('');
+
+    const handleTextChange = (event) => {
+        setInputText(event.target.value);
+    };
+
+    const handleConvertClick = () => {
+        setConvertedText(inputText.toUpperCase());
+    };
+
+    const handleConvertClickL = () => {
+        setConvertedText(inputText.toLowerCase());
+    };
+
+    const handlecleartext = () => {
+        setConvertedText('');
+        setInputText('');
+    };
+
+    return (
+        <div style={{ textAlign: 'center', backgroundColor: "##ffe7e7"}}>
+            <textarea value={inputText} style={{"width": "500px", "height": "100px", marginTop: "20px"}} onChange={handleTextChange} />
+            <br /> <br />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button onClick={handleConvertClick}>Convert to Uppercase</button>
+            <button onClick={handleConvertClickL}>Convert to Lowercase</button>
+            <button onClick={handlecleartext}>Clear Text</button>
+            </div>
+            <br /> <br />
+            <p>{convertedText}</p>
+        </div>
+    );
+}
+
+export default TextConverter;
